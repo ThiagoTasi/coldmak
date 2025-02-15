0012013f3a96724a6eafd12a0ada7b096f12fe04
Thiago Tasi <thiagosilvateo@gmail.com>
2025-02-15 18:31:17 -0300

    Adicionar arquivos de projeto.

---
+++ banco.cs
@@ -0,0 +1,33 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysTINSClass
+{
+    // classe Banco é nossa classe de conexão
+    public static class Banco // static pois não precisaremos criar uma instancia de Banco para conectar às nossas base dados
+    {
+        public static MySqlCommand Abrir(int cod = 0) // método para abrir conexão
+        {
+            string strcon = @"server=127.0.0.1;database=coldmak;user=root;password=";
+            MySqlConnection cn = new(strcon);
+            MySqlCommand cmd = new();
+            try
+            {
+                cn.Open();
+                cmd.Connection = cn;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            return cmd;
+        }
+    }
+}
---
+++ usuario.cs
@@ -0,0 +1,170 @@
+﻿
+using coldmak;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
+namespace coldmak
+{
+    public class usuario
+    {
+        public int Id;
+        public string? Nome;
+        public string? Rg;
+        public string? Cpf;
+        public string? Endereco;
+        public string? Cep;
+        public string? Email;
+        public string? Telefone;
+
+        public usuario()
+        {
+            Id = 0;
+        }
+
+        public usuario(string nome, string rg, string cpf, string endereco, string cep, string email)
+        {
+            Nome = nome;
+            Rg = rg;
+            Cpf = cpf;
+            Endereco = endereco;
+            Cep = cep;
+            Email = email;
+
+        }
+        public usuario(string nome, string rg, string cpf, string endereco, string cep, string email, string telefone)
+        {
+            Nome = nome;
+            Rg = rg;
+            Cpf = cpf;
+            Endereco = endereco;
+            Cep = cep;
+            Email = email;
+            Telefone = telefone;
+        }
+        public usuario(int id, string nome, string rg, string cpf, string endereco, string cep, string email, string telefone)
+        {
+            Id = id;
+            Nome = nome;
+            Rg = rg;
+            Cpf = cpf;
+            Endereco = endereco;
+            Cep = cep;
+            Email = email;
+            Telefone = telefone;
+        }
+
+    }
+}
+
+//inserir usuario
+public void Inserir()
+{
+    var cmd = Banco.Abrir();
+    cmd.CommandType = CommandType.StoredProcedure;
+    cmd.CommandText = "sp_usuario_insert";
+    cmd.Parameters.Add("spnome", MySql.Data.MySqlClient.MySqlDbType.VarChar).Value = Nome;
+    cmd.Parameters.AddWithValue("sprg", Rg);
+    cmd.Parameters.AddWithValue("spcpf", Cpf);
+    cmd.Parameters.AddWithValue("spendereco", Endereco);
+    cmd.Parameters.AddWithValue("spcep", Cep);
+    cmd.Parameters.AddWithValue("spemail", Email);
+    cmd.Parameters.AddWithValue("sptelefone", Telefone);
+
+    var dr = cmd.ExecuteReader();
+    if (dr.Read())
+    {
+        Id = dr.GetInt32(0);
+    }
+    cmd.Connection.Close();
+}
+//Obter por id
+public static Usuario ObterPorId(int id)
+{
+    Usuario usuario = new Usuario();
+    var cmd = Banco.Abrir();
+    cmd.CommandText = $"select * from usuarios where id = {id}";
+    var dr = cmd.ExecuteReader();
+    if (dr.Read())
+    {
+        usuario = new(
+            dr.GetInt32(0),
+            dr.GetString(1),
+            dr.GetString(2),
+            dr.GetString(3),
+            dr.GetString(4),
+            dr.GetString(5),
+            dr.GetString(6),
+            dr.GetString(7)
+
+
+            );
+    }
+    return usuario;
+}
+public static List<Usuario> ObterLista()
+{
+    List<Usuario> lista = new();
+    var cmd = Banco.Abrir();
+    cmd.CommandText = $"select * from usuarios order by nome asc";
+    var dr = cmd.ExecuteReader();
+    while (dr.Read())
+    {
+        lista.Add(new(
+            dr.GetInt32(0),
+            dr.GetString(1),
+            dr.GetString(2),
+            dr.GetString(3),
+            dr.GetString(4),
+            dr.GetString(5),
+            dr.GetString(6),
+            dr.GetString(7)
+
+            )
+        );
+    }
+
+    return lista;
+}
+public bool Atualizar()
+{
+    var cmd = Banco.Abrir();
+    cmd.CommandType = CommandType.StoredProcedure;
+    cmd.CommandText = "sp_usuario_update";
+    cmd.Parameters.AddWithValue("spid", Id);
+    cmd.Parameters.AddWithValue("spnome", Nome);
+    cmd.Parameters.AddWithValue("sprg", Rg);
+    cmd.Parameters.AddWithValue("spcpf", Cpf);
+    cmd.Parameters.AddWithValue("spendereco", Endereco);
+    cmd.Parameters.AddWithValue("spcep", Cep);
+    cmd.Parameters.AddWithValue("spemail", Email);
+    cmd.Parameters.AddWithValue("sptelefone", Telefone);
+
+}
+//efetuar login
+public static Usuario EfetuarLogin(string email, string senha)
+{
+    Usuario usuario = new();
+    var cmd = Banco.Abrir();
+    cmd.CommandText = $"select * from usuarios where email = '{email}' and senha = md5('{senha}') and ativo = 1";
+    var dr = cmd.ExecuteReader();
+    if (dr.Read())
+    {
+        usuario = new(
+            dr.GetInt32(0),
+            dr.GetString(1),
+            dr.GetString(2),
+            dr.GetString(3),
+            dr.GetString(4),
+            dr.GetString(5),
+            dr.GetString(6),
+            dr.GetString(7)
+           );
+    }
+    return usuario;
+}
